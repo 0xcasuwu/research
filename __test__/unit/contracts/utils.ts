21a666167556f5f94893a6d6a91ee42a07f643c4
casuwu <ed995499@gmail.com>
2025-04-16 15:41:08 -0400

    prune

--- __test__/unit/contracts/utils.ts
+++
@@ -1,5 +0,0 @@
-import { ABICoder } from '@btc-vision/transaction';
-
-export function encodeNumericSelector(selector: string): number {
-  return Number(`0x${new ABICoder().encodeSelector(selector)}`);
-}
